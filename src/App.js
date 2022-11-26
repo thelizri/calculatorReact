03831509c710a5408dc63a928f4344ba864527cb
thelizri <karl.william.tony.carlstedt@gmail.com>
2022-11-26 01:56:55 +0100

    Second Commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,46 @@
 import "./styles.css";
+import {useReducer} from "react";
+import DigitButton from "./DigitButton";
+import OperationButton from "./OperationButton";
+
+export const ACTIONS = {
+  ADD_DIGIT: 'add_digit',
+  CHOOSE_OPERATION: 'choose_operation',
+  CLEAR: 'clear',
+  DELETE_DIGIT: 'delete_digit',
+  EVALUATE: 'evaluate'
+}
+
+function reducer(state, {type, payload}){
+
+}
 
 function App() {
+
+  const [{currentOperand, previousOperand, operator},dispatch] = useReducer(reducer, {});
+
   return ( <div className={"calculatorGrid"}>
     <div className={"output"}>
-      <div className={"previous_operand"}>Number</div>
-      <div className={"current_operand"}>10000</div>
+      <div className={"previous_operand"}>{previousOperand}</div>
+      <div className={"current_operand"}>{currentOperand}</div>
     </div>
     <button className={"span_two"}>AC</button>
     <button>DEL</button>
-    <button>÷</button>
-    <button>1</button>
-    <button>2</button>
-    <button>3</button>
-    <button>*</button>
-    <button>4</button>
-    <button>5</button>
-    <button>6</button>
-    <button>+</button>
-    <button>7</button>
-    <button>8</button>
-    <button>9</button>
-    <button>-</button>
-    <button>.</button>
-    <button>0</button>
+    <OperationButton dispatch={dispatch} operation={"÷"}/>
+    <DigitButton dispatch={dispatch} digit={"1"}/>
+    <DigitButton dispatch={dispatch} digit={"2"}/>
+    <DigitButton dispatch={dispatch} digit={"3"}/>
+    <OperationButton dispatch={dispatch} operation={"*"}/>
+    <DigitButton dispatch={dispatch} digit={"4"}/>
+    <DigitButton dispatch={dispatch} digit={"5"}/>
+    <DigitButton dispatch={dispatch} digit={"6"}/>
+    <OperationButton dispatch={dispatch} operation={"+"}/>
+    <DigitButton dispatch={dispatch} digit={"7"}/>
+    <DigitButton dispatch={dispatch} digit={"8"}/>
+    <DigitButton dispatch={dispatch} digit={"9"}/>
+    <OperationButton dispatch={dispatch} operation={"-"}/>
+    <DigitButton dispatch={dispatch} digit={"."}/>
+    <DigitButton dispatch={dispatch} digit={"0"}/>
     <button className={"span_two"}>=</button>
   </div>);
 }
